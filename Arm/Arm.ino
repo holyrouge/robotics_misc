2b431a7102359246331d322a54bc8619e8d8b5d0
Joseph Cullen <joseph.cullen@stonybrook.edu>
2018-03-07 15:11:46 -0500

    controller_radio.inio takes in controller data to move rover. Arm.ino unfinished.

---
+++ Arm/Arm.ino
@@ -0,0 +1,170 @@
+
+/*
+ * SBU Robot Design Team
+ * 
+ * Implements a way to control MX 12 servo.
+ * 
+ * Author: Sasha Sokolov
+ * Date: 10/9/2017                                                PUT RIGHT TIME STAMP
+ */
+ 
+#define ARM_PACKET_SIZE 7
+#define HEADER 0
+#define BASE_ARM 1 //  D left, dright
+#define FOREARM 2  // Dup, Ddown
+#define WRIST_PITCH 3 // LT , RT
+#define WRIST_ROTATION 4 // LB, RB
+#define HAND_CLOSE 5 //A/B
+#define CHECKSUM 6 
+
+#define BASE_ARM_SPEED 4
+#define BASE_ARM_DIR1 3
+#define BASE_ARM_DIR2 2
+
+#define FOREARM_SPEED 0
+#define FOREARM_DIR1 0
+#define FOREARM_DIR2 0
+
+#define WRIST_PITCH_ID 1
+#define WRIST_ROTATION_ID 2
+#define HAND_CLOSE_PIN_ID 3
+
+#define LINEAR_ACT_MOVESPEED 60
+#define CONTINUOUS_ROTATION_SPEED_FORWARDS 300
+#define CONTINUOUS_ROTATION_SPEED_BACKWARDS 1024+300
+
+void setup() 
+{
+  pinMode(BASE_ARM_SPEED, OUTPUT);
+  pinMode(BASE_ARM_DIR1, OUTPUT);
+  pinMode(BASE_ARM_DIR2, OUTPUT);
+  
+  //pinMode(FOREARM_SPEED, OUTPUT);
+  //pinMode(FOREARM_DIR1, OUTPUT);
+  //pinMode(FOREARM_DIR2, OUTPUT);
+  
+  Serial1.begin(1000000);           //AX Motor             //Preparing Serial1 pin with baud rate of 1000000
+  Serial2.begin(1000000);           //AX Motor
+  Serial3.begin(115200);    //RADIO                   
+  Serial.begin(115200);
+  Serial.flush();
+}
+
+int index = 0;
+char input_data[ARM_PACKET_SIZE];
+
+void loop() 
+{
+  //Read from radio
+  if(Serial3.available()) {
+    int data = Serial3.read();
+      if(index != 0 && data == 0xFF) {
+         //Misaligned... ignore data until we get 0xFF for start of next packet
+         index = 0;
+         return;
+      }
+      if(index == 0 && data != 0xFF) {
+        //misaligned... throw out data until this is true;
+        return;
+      }
+      input_data[index] = data;
+      index++;
+    //Serial.println((int) data);
+  }
+
+  //We got a full packet
+  if(index == 7) {
+
+    char sum = 0;
+    for(int i = 1; i < 6; i++) {
+      sum+= input_data[i];
+    }
+    sum += 0xBB;
+    if(sum == input_data[CHECKSUM]) {
+      for(int i = 0; i < ARM_PACKET_SIZE; i++) {
+        //Serial.print((unsigned char)input_data[i]);
+        //Serial.print(", ");
+      }
+      //Serial.println();
+
+     // moveLinearAct(BASE_ARM_SPEED, BASE_ARM_DIR1, BASE_ARM_DIR2, input_data[BASE_ARM]);
+//      moveLinearAct(FOREARM_SPEED, FOREARM_DIR1, FOREARM_DIR2, input_data[FOREARM]);
+//      //manual_move(WRIST_PITCH_ID, WRIST_PITCH_PIN, input_data[WRIST_PITCH]);
+      continuousRotation(WRIST_ROTATION_ID, input_data[WRIST_ROTATION]);
+//      continuousRotation(HAND_CLOSE_PIN_ID, input_data[HAND_CLOSE]);
+      
+      
+    } else {
+      //Serial.println("checksum fail");
+    }
+    index = 0;
+  }
+}
+
+
+void continuousRotation(int id, int dir_sig) {
+ if(dir_sig == 0) {
+  AX(id, 0);
+ } else if (dir_sig == 1) {
+    AX(id, CONTINUOUS_ROTATION_SPEED_FORWARDS);
+ } else if (dir_sig == 2) {
+    AX(id, CONTINUOUS_ROTATION_SPEED_BACKWARDS);
+ }
+}
+
+void moveLinearAct(int speed_pin, int dir1_pin, int dir2_pin, int dir_sig) {
+ if(dir_sig == 0) {
+  digitalWrite(dir1_pin, LOW);
+  digitalWrite(dir2_pin, LOW);
+  digitalWrite(speed_pin, 0);
+ } else if (dir_sig == 1) {
+    digitalWrite(dir1_pin, LOW);
+    digitalWrite(dir2_pin, HIGH);
+    digitalWrite(speed_pin, 1);
+ } else if (dir_sig == 2) {
+    digitalWrite(dir2_pin, LOW);
+    digitalWrite(dir1_pin, HIGH);
+    digitalWrite(speed_pin, 1);
+ }
+}
+
+//0 to 4095 for position
+void MX(int id, int com)
+{
+  byte data[8] = {255, 255, id, 5, 3, 30, lowByte(com), highByte(com)};
+  sendData(data, sizeof(data));
+}
+
+//0 to 2047 for speed (0 to 1023 CCW (0 stop, 1023 max speed), 1024 to 2047 CW (1024 stop, 2047 max speed))
+void AX(int id, int com)
+{
+  byte data[8] = {255, 255, id, 5, 3, 32, lowByte(com), highByte(com)};
+  sendData(data, sizeof(data));
+}
+
+void sendData(byte data[], int len)
+{
+  byte sum = 0;
+  for (int i = 2; i < len; i++)
+  {
+    sum += data[i];                             //Adding up the data values to get checksum
+  }
+  Serial1.write(data, len);                     //Sending the data
+  Serial1.write(~sum);                          //Sending the checksum
+  Serial1.end();                                //Disabling Serial1 so servo can respond
+  getData();
+}
+void getData()
+{
+  while(Serial2.available())
+  {
+    Serial.println(Serial2.read());             //Reading echo
+  }
+  while(!Serial2.available()){}                 //Waiting for status packet
+  while(Serial2.available())
+  {
+    Serial.println(Serial2.read());             //Reading status packet
+  }
+  Serial.println();
+  Serial1.begin(1000000);                       //Preparing Serial1 pin with baud rate of 1000000
+}
---
+++ controller_radio/controller_radio.ino
@@ -0,0 +1,198 @@
+//Controls the robot. 2/23/18
+/*
+ * Control the robot. Connect radio usb to power, and DB9 cable
+ * for data to lab computer. 
+ * Open Putty. Set COM port appropriately (check in devices) 
+ * and baud rate to 115200
+ * Control via following 
+ * 1 - 25% forward
+ * 2 - 50% forward
+ * 3 - 75% forward
+ * 4 - 100% forward
+ * 5 - 25% backwards
+ * 6 - 50% backwards
+ * 7 - 75% backwards
+ * 8 - 100% backwards
+ * 0 - Stop
+ * 
+ * This is not apparent in code because data is coming in corrupted.
+ * To be fixed in future update
+ *  
+ */
+
+
+
+//front left
+const int dir_1_pin = 6;
+const int m1_speed_pin = 7;
+const int m1_sign = 1;
+
+//front right
+const int dir_2_pin = 9;
+const int m2_speed_pin = 10;
+const int m2_sign = 1;
+
+//back left
+const int dir_3_pin = 2;
+const int m3_speed_pin = 3;
+const int m3_sign = 1;
+
+//back right
+const int dir_4_pin = 12;
+const int m4_speed_pin = 13;
+const int m4_sign = 1;
+
+const int COUNTER_MAX = 250;
+
+int target_speed_1 = 0;
+int actual_speed_1 = 0;
+
+int target_speed_2 = 0;
+int actual_speed_2 = 0;
+
+int target_speed_3 = 0;
+int actual_speed_3 = 0;
+
+int target_speed_4 = 0;
+int actual_speed_4 = 0;
+
+int counter = 0;
+
+#define HEADER     0
+
+#define FRONT_LEFT 1
+#define FRONT_RIGHT 2
+
+#define MID_LEFT 3
+#define MID_RIGHT 4
+
+#define BACK_LEFT 5
+#define BACK_RIGHT 6
+
+#define CHECKSUM 7
+
+char input_data[8];
+int index = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial2.begin(9600);
+  Serial3.begin(115200); //direct line to radio
+  
+  pinMode(dir_1_pin, OUTPUT);
+  pinMode(m1_speed_pin, OUTPUT);
+  pinMode(dir_2_pin, OUTPUT);
+  pinMode(m2_speed_pin, OUTPUT);
+  pinMode(dir_3_pin, OUTPUT);
+  pinMode(m3_speed_pin, OUTPUT);
+  pinMode(dir_4_pin, OUTPUT);
+  pinMode(m4_speed_pin, OUTPUT);
+}
+
+void read_radio() {
+    // put your main code here, to run repeatedly:
+  if(Serial3.available()) {
+    int data = Serial3.read();
+      if(index != 0 && data == 0xFF) {
+         //Misaligned... ignore data until we get 0xFF for start of next packet
+         index = 0;
+         return;
+      }
+      if(index == 0 && data != 0xFF) {
+        //misaligned... throw out data until this is true;
+        return;
+      }
+      input_data[index] = data;
+      index++;
+    //Serial.println((int) data);
+  }
+
+  if(index == 8) {
+
+    char sum = 0;
+    for(int i = 1; i < 7; i++) {
+      sum+= input_data[i];
+    }
+    sum += 0xAA;
+    if(sum == input_data[CHECKSUM]) {
+      for(int i = 0; i < 8; i++) {
+        //Serial.print((int)input_data[i]);
+        //Serial.print(", ");
+      }
+      //Serial.println();
+      //checksum passed, set fields
+      target_speed_1 = input_data[FRONT_LEFT] * 2;
+      target_speed_2 = input_data[FRONT_RIGHT] * 2;
+      target_speed_3 = input_data[BACK_LEFT] * 2;
+      target_speed_4 = input_data[BACK_RIGHT] * 2;
+      
+    } else {
+      //Serial.println("checksum fail");
+    }
+    index = 0;
+  }
+}
+
+void loop() {
+  read_radio();
+
+//    // Do nothing we are at the right speed
+//    if(target_speed == actual_speed) {
+//     // Serial.println("reached target");
+//      return;
+//    }
+
+    // Stall using counter as timer
+    if(counter < COUNTER_MAX) {
+      counter++;
+    }
+
+    // Adjust speed when counter is maxed, reset counter
+    if(counter % COUNTER_MAX == COUNTER_MAX-1) {
+      counter = 0;
+      if(actual_speed_1 != target_speed_1) {
+        actual_speed_1 < target_speed_1 ? actual_speed_1++: actual_speed_1--;
+      }
+      if(actual_speed_2 != target_speed_2) {
+        actual_speed_2 < target_speed_2 ? actual_speed_2++: actual_speed_2--;
+      }
+      if(actual_speed_3 != target_speed_3) {
+        actual_speed_3 < target_speed_3 ? actual_speed_3++: actual_speed_3--;
+      }
+      if(actual_speed_4 != target_speed_4) {
+        actual_speed_4 < target_speed_4 ? actual_speed_4++: actual_speed_4--;
+      }
+      
+    } else {
+      return; //dont waste sending same val as before
+    }
+
+  Serial.print("Front Left:");
+  set_pwm(dir_1_pin, m1_speed_pin, m1_sign, actual_speed_1);
+  Serial.print(" Front Right:");
+  set_pwm(dir_2_pin, m2_speed_pin, m2_sign, actual_speed_2);
+  Serial.print(" Back Left:");
+  set_pwm(dir_3_pin, m3_speed_pin, m3_sign, actual_speed_3);
+  Serial.print(" Back Right:");
+  set_pwm(dir_4_pin, m4_speed_pin, m4_sign, actual_speed_4);
+  Serial.println();
+   
+    
+}
+
+void set_pwm(int dir_pin, int m_speed_pin, int sign, int m_actual_speed) {
+  if((m_actual_speed * sign) >= 0) {
+    digitalWrite(dir_pin, HIGH);
+    Serial.print("+");
+    analogWrite(m_speed_pin, m_actual_speed*sign);
+    Serial.print(m_actual_speed*sign);
+  } else {
+    Serial.print("-");
+    digitalWrite(dir_pin, LOW);
+    analogWrite(m_speed_pin, -1*m_actual_speed*sign);
+    Serial.print(-1*m_actual_speed*sign);
+  }
+}
+
